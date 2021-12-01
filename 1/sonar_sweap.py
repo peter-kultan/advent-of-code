5a49aade7cc6bb7a3a0b5fe560b05f16f678533b
Peter Kultán <xkultan@fi.muni.cz>
2021-12-01 10:41:01 +0100

    first day done

--- 1/sonar_sweap.py
+++ 1/sonar_sweap.py
@@ -1,3 +1,6 @@
+from typing import List, Optional
+
+
 def determine_depth(path: str) -> int:
     result = 0
     with open(path) as my_file:
@@ -9,5 +12,22 @@ def determine_depth(path: str) -> int:
     return result
 
 
+def determine_depth_sum(path: str) -> int:
+    result = 0
+    numbers: List[int] = []
+    previous_depth: Optional[int] = None
+    with open(path) as my_file:
+        for line in my_file:
+            numbers.append(int(line.rstrip()))
+            if len(numbers) == 3:
+                if previous_depth is not None and\
+                        sum(numbers) > previous_depth:
+                    result += 1
+                previous_depth = sum(numbers)
+                numbers = numbers[1:]
+    return result
+
+
 if __name__ == "__main__":
     print(determine_depth("input.txt"))
+    print(determine_depth_sum("input_measurement_windows.txt"))
