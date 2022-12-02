548e4acc049c334ce0ea5482b460b86e311c5928
Peter Kultán <xkultan@fi.muni.cz>
2022-12-02 20:58:16 +0100

    feat: implements day01

---
+++ 2022/day01/day01/CalorieCounting.cs
@@ -0,0 +1,46 @@
+﻿namespace day01;
+
+public static class CalorieCounting
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter file path:");
+        
+        string path = Console.ReadLine();
+
+        if (!File.Exists(path))
+        {
+            Console.WriteLine("File doesn't exists");
+            return;
+        }
+
+        Console.WriteLine(ParseCalories(path));
+    }
+
+    public static int ParseCalories(string path)
+    {
+        int currentCount = 0;
+        int currentOrderNum = 1;
+        int mostCount = 0;
+        int mostOrderNum = 0;
+
+        foreach (var line in File.ReadLines(path))
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                if (currentCount > mostCount)
+                {
+                    mostCount = currentCount;
+                    mostOrderNum = currentOrderNum;
+                }
+                currentCount = 0;
+                currentOrderNum++;
+            }
+            else
+            {
+                currentCount += Int32.Parse(line);
+            }
+        }
+        return mostCount;
+    }
+}
