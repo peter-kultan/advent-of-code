9bf654b2676de86dc67024804cf58d16b83fbef4
Peter Kultan <Peter.Kultan@Honeywell.com>
2022-12-06 09:18:11 +0100

    feat: implements day06

---
+++ 2022/day06/day06/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace day06;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Write path to input file:");
+
+        var input = Console.ReadLine();
+
+        if (!File.Exists(input))
+        {
+            Console.WriteLine("File doesn't exists");
+            return;
+        }
+
+
+        Console.WriteLine(TuningTrouble.FindMarker(input, 4));
+        Console.WriteLine(TuningTrouble.FindMarker(input, 14));
+    }
+}
+
+public static class TuningTrouble
+{
+    public static int FindMarker(string path, int nubmerOfDistinctLetters)
+    {
+        var queue = new Queue<char>();
+        var position = 0;
+        using (StreamReader sr = new StreamReader(path))
+        {
+            while (sr.Peek() >= 0)
+            {
+                queue.Enqueue((char)sr.Read());
+                position++;
+
+                if (queue.Count == nubmerOfDistinctLetters)
+                {
+                    if (queue.ToHashSet().Count() == nubmerOfDistinctLetters)
+                    {
+                        break;
+                    }
+                    queue.Dequeue();
+                }
+            }
+        }
+        return position;
+    }
+}
